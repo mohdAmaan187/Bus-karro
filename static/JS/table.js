9f9482f50b848041e70a11c87261348b19d7f392
mohdAmaan187 <mohdamaanshaikh1807@gmail.com>
2025-01-05 11:50:35 +0530

    new updates

--- static/JS/table.js
+++ static/JS/table.js
@@ -21,3 +21,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#edit_buses"); // Select the table
+    const editButton = document.querySelector(".edt"); // Select the search button
+
+    // Initially hide the table
+    form.style.display = "none";
+
+    // Add event listener to the search button
+    editButton.addEventListener("click", (event) => {
+        event.preventDefault(); // Prevent default button behavior (for <a> or <button> inside <a>
+            form.style.display = "flex";
+    });
+});
